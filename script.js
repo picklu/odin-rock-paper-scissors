9c6d21bb6993c3ca130fc9d33f843760db6b4e33
Subrata Sarker <subrata_sarker@yahoo.com>
2025-04-03 08:20:09 +0900

    Add pseudo code to the script

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Welcome to the Rock Paper Scissors game!");
+// Initialize the score
+// Prompt the user for their choice
+// Get the random choice of the computer
+// Compare the choices and determine the winner
+// Display the result
+// Ask the user if they want to play again
+// If the user chooses to play again, restart the game
+// If the user chooses not to play again, end the game
+// Display the final score
+// Thank the user for playing
+// End the game
