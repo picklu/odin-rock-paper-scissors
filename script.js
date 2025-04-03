6f11e17b708538d004ac5819fa9f785bc00d53e4
Subrata Sarker <subrata_sarker@yahoo.com>
2025-04-03 21:58:42 +0900

    Write the logic to get the computer choice
    Write the logic to get the computer choice inside getComputerChoice
    function.

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ console.log("Welcome to the Rock Paper Scissors game!");
 // Initialize the score
 // Prompt the user for their choice
 // Get the random choice of the computer
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
 // Compare the choices and determine the winner
 // Display the result
 // Ask the user if they want to play again
